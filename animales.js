4e3b1ab742aa07ccb3ecf20f8a486acf75533484
Diso98 <matias.paradiso@kyndryl.com>
2022-11-23 21:11:10 -0300

    mis cambios

--- animales.js
+++ animales.js
@@ -1,3 +1,14 @@
 "Perro";
 "Gato";
 "Caballo"
+console.log("hola mundo")
+
+let razas = ["labrador", "boxer"]
+let elegir = function(razas){
+    if (razas = "labrador")
+    return "Que perro lindo"
+    else 
+    return 
+
+}
+console.log(elegir(razas))
